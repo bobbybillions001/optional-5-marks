c8446e6e0fa07b9577c390a7bda761e78c4f66c6
bobbybillions001 <bobby-olumide.adefisoye@binghamuni.edu.ng>
2024-12-04 16:08:38 +0100

    first commit

---
+++ main.py
@@ -0,0 +1,11 @@
+number_list = [1,3,67,45,29,89,45,21,54,60]
+
+index = 0
+while number_list[index:]:
+    index +=1
+
+    num_index = index - 1
+
+for number in range(num_index, -1, -1):
+
+    print(number_list[number])
